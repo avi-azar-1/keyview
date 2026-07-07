7329a4db4f124e9f42d4c4c4ed5ed597f08b3fc9
tx6417 <tx6417@bluesky2525.com>
2026-07-07 14:37:53 +0300

    incresed batch sizes

--- backend/app/config.py
+++ backend/app/config.py
@@ -6,8 +6,8 @@ AUTO_DOMAIN = ".temp.com"
 
 class Settings(BaseSettings):
     static_dir: str = "./static"
-    redis_scan_count: int = 1000
-    redis_pipeline_batch: int = 100
+    redis_scan_count: int = 10000
+    redis_pipeline_batch: int = 10000
 
 
 settings = Settings()
